851e3144639b3be871db444b84fadf7584676875
Salino3 <flaviobcn012@gmail.com>
2025-01-25 09:25:01 +0100

    create first component

---
+++ src/components/home/home-page.jsx
@@ -0,0 +1,8 @@
+import "./home-page.styles.scss";
+export const HomePage = () => {
+  return (
+    <div className="containerHome">
+      <p>This is a simple React application.</p>
+    </div>
+  );
+};
