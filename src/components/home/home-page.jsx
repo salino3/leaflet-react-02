f6887dd0aefd93a931acbf5e395d8595623e2adc
Salino3 <flaviobcn012@gmail.com>
2025-01-25 09:58:39 +0100

    show map

--- src/components/home/home-page.jsx
+++ src/components/home/home-page.jsx
@@ -14,7 +14,12 @@ export const HomePage = () => {
     <div className="containerHome">
       <p>This is a simple Leaflet React application.</p>
       <div className="bodyHomePage">
-        <MapContainer center={center} zoom={ZOOM_LEVEL} ref={mapRef}>
+        <MapContainer
+          center={center}
+          zoom={ZOOM_LEVEL}
+          style={{ minHeight: "90vh", width: "100%" }}
+          ref={mapRef}
+        >
           <TileLayer
             url={osm?.maptiler?.url}
             attribution={osm?.maptiler?.attribution}
